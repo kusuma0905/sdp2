ca06de5aa76aa003b8468f98ef7f330428b0b27d
kusuma0905 <22000651@kluniversity.in>
2024-03-09 18:57:53 +0530

    signup added

---
+++ react/first/src/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+import logo from './images/logo.jpg';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = () => {
+        // Add your submission logic here
+        console.log('Submitted');
+    };
+
+    return (
+        <div className="LoginComponent">
+            <div className="LoginHead">
+                <h1>ACADEMIC PROJECT MANAGEMENT SYSTEM</h1>
+            </div>
+            <div className="LoginComponent-in">
+                <div className="Login-img">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="Login-one">
+                    <input
+                        type="text"
+                        placeholder="Enter your email"
+                        name="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        required
+                    />
+                </div>
+                <div className="Login-two">
+                    <input
+                        type="password"
+                        placeholder="Enter your password"
+                        name="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        required
+                    />
+                </div>
+                <div className="Login-three">
+                    <button onClick={handleSubmit}>Submit</button>
+                </div>
+                <div className="Login-four">
+                    <p>Already have an Account ?</p> <Link to="/register">Register</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ react/first/src/Register.js
@@ -0,0 +1,223 @@
+import React from 'react'
+import { useState, useEffect} from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom';
+
+import './Register.css'
+
+
+const Sign = () => {
+
+    const [userData, setUserData] = useState({
+        firstName: '',
+        lastName: '',
+        username: '',
+        email: '',
+        gender: '',
+        password: '',
+        branch: '',
+        confirmPassword: '',
+        phone: '',
+        address: '',
+        residence: '',
+        profile_pic: 'NA',
+    })
+
+    const handleChange = (e) => {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleBranch = (e) => {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async () => {
+        try {
+            if(userData.password !== userData.confirmPassword) {
+                alert('passwords do not match')
+                return
+            }
+             
+            if (!userData.firstName || !userData.lastName || !userData.username || !userData.email || !userData.gender || !userData.password || !userData.branch || !userData.confirmPassword || !userData.phone || !userData.address || !userData.residence || !userData.profile_pic) {
+                alert('Please fill all the fields')
+                return
+            }
+            
+            const response = await axios.post('/api/auth/signup', userData)
+            
+            if(response.data.status === 200) {
+                // toast.success('Account created successfully')
+            }
+            if(response.data.status === 201) {
+                // toast.error('Invalid Username')
+            }
+            if(response.data.status === 204) {
+                // toast.error('Invalid year')
+            }
+            if(response.data.status === 500) {
+                // toast.error('Internal server error')
+            }
+            
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+  return (
+        <div className="SignComponent">
+            <div className="SignComponent-in">
+
+                <div className="SignComponent-in-in">
+                    
+                    <div className="Sign-up-one">
+                        <div className="Sign-up-one-one">
+                            <input 
+                                type="text" 
+                                placeholder='firstName'
+                                name='firstName'
+                                value={userData.firstName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="Sign-up-one-two">
+                            <input 
+                                type="text" 
+                                placeholder='lastName'
+                                name='lastName'
+                                value={userData.lastName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="Sign-up-two">
+                        <div className="Sign-up-two-one">
+                            <input 
+                                type="text" 
+                                placeholder='username'
+                                name='username'
+                                value={userData.username}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="Sign-up-two-two">
+                            <input 
+                                type="text" 
+                                placeholder='email'
+                                name='email'
+                                value={userData.email}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+
+
+                    <div className="Sign-up-three">
+                        <div className="Sign-up-three-one">
+                            <select 
+                                name="gender"
+                                value={userData.gender}
+                                onChange={handleBranch} 
+                            >   
+                                <option value="">Select Gender</option>
+                                <option value="Male">Male</option>
+                                <option value="FeMale">FeMale</option>
+
+                            </select>
+                        </div>
+                        <div className="Sign-up-three-two">
+                            <select 
+                                name="residence"
+                                value={userData.residence}
+                                onChange={handleBranch} 
+                            >   
+                                <option value="">Select Residence</option>
+                                <option value="H">Hostler</option>
+                                <option value="D">DayScholar</option>
+
+                            </select>
+                        </div>
+                        <div className="Sign-up-three-three">
+                            <input 
+                                type="text" 
+                                placeholder='address'
+                                name='address'
+                                value={userData.address}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+
+
+                    <div className="Sign-up-four">
+                        <div className="Sign-up-four-one">
+                            <select 
+                                name="branch"
+                                value={userData.branch}
+                                onChange={handleBranch} 
+                            >   
+                                <option value="">Select Branch</option>
+                                <option value="CSE">CSE</option>
+                                <option value="ECE">ECE</option>
+                                <option value="EEE">EEE</option>
+
+                            </select>
+                        </div>
+
+                        <div className="Sign-up-four-two">
+                            <input 
+                                type="text"
+                                placeholder='password'
+                                name='password'
+                                value={userData.password}
+                                onChange={handleChange} 
+                            />
+                        </div>
+                        <div className="Sign-up-four-three">
+                            <input 
+                                type="text"
+                                placeholder='confirmPassword'
+                                name='confirmPassword'
+                                value={userData.confirmPassword}
+                                onChange={handleChange} 
+                            />
+                        </div>
+                    </div>
+
+
+                    <div className="Sign-up-five">
+                        <div className="Sign-up-five-one">
+                            <input 
+                                type="text" 
+                                placeholder='phone'
+                                name='phone'
+                                value={userData.phone}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="Sign-up-five-two">
+                            <button onClick={handleSubmit}>SignUp</button>    
+                        </div>
+                    </div>  
+
+                </div>
+
+                <div className="SignComponent-login">
+                    <p>Already have an account?</p>
+                    <Link to="/">Login</Link>
+                </div>
+
+            </div>
+        </div>
+  )
+}
+
+export default Sign
